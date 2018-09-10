1c6a5312d4ba87c191b5bd8d0493aad23a4cb7cc
asadkhan404 <asad.khan@aasaanjobs.com>
2018-09-10 13:34:00 +0530

    API-869 Add basic python script
    >> 4 steps involved in the task:
    * Get all IP addresses with google sdk
    * Scan the list using nmap in shell command
    * Save all outputs to text file
    * Send the text contents through sendgrid to AJ admins

---
+++ src/scanner.py
@@ -0,0 +1,92 @@
+# Python script to get IP address of all project vm instances
+
+import os
+
+import time
+import googleapiclient.discovery
+import sendgrid
+
+from datetime import datetime
+from sendgrid.helpers.mail import *
+
+project = "aj-cloud-staging"
+region = "asia-south1"
+
+
+def get_instance_ip_address_list():
+    print("\nAccessing google client API\n")
+    compute = googleapiclient.discovery.build('compute', 'v1')
+
+    print("Getting Addresses")
+    request = compute.addresses().list(project=project, region=region)
+    api_result = request.execute()
+    return api_result
+
+
+def execute_port_scan(api_result):
+    if api_result is not None:
+        ip_list = api_result['items']
+
+        print_ip_metadata(ip_list)
+
+        for item in ip_list:
+            ip = item['address']
+            command = "nmap -sV -p- " + ip + " -Pn"
+            os.system('output="$(' + command + ')"; echo $output >> output.txt; echo "\n" >> output.txt')
+
+        print("Completed scans of all IPs. Please find report in output.txt")
+        return True
+    else:
+        print("IP addresses couldn't be retrieved... Error occurred")
+        print(api_result)
+        return False
+
+
+def print_ip_metadata(ip_list):
+    print("Addresses to be scanned:")
+    for item in ip_list:
+        ip = item['address']
+        name = item['name']
+        instance_id = item['id']
+        print("Instance: " + name + "  -  IP Address: " + ip + "  -  ID: " + instance_id)
+
+
+def send_report(file_contents):
+    sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+
+    timestamp = time.time()
+    time_now = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+    print("Time now: ", time_now)
+
+    from_email = Email("test@example.com")
+    to_email = Email("test@example.com")
+    subject = "Open port scanner report - " + time_now
+    mail_content = Content("text/plain", file_contents)
+    mail_draft = Mail(from_email, subject, to_email, mail_content)
+    response = sg.client.mail.send.post(request_body=mail_draft.get())
+
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+
+    # Clear file contents
+    os.system('echo "\n" > output.txt')
+
+
+def get_output_content():
+    with open("output.txt", "r") as output_file:
+        file_content = output_file.read()
+        print("File Content: ", file_content)
+        return file_content
+
+
+if __name__ == "__main__":
+    api_response = get_instance_ip_address_list()
+    debug = False
+    scan_success = execute_port_scan(api_response)
+    if scan_success:
+        content = get_output_content()
+        if debug:
+            send_report(content)
+    else:
+        print("Scanning Failed!")
