ad59f83364d986e1841eb4bac156c67cb79015e0
asadkhan404 <asad.khan@aasaanjobs.com>
2018-09-11 17:33:21 +0530

    API-689 Code refactoring
    * Break down monolithic method
    * Fix output data format
    * Add better script running status output
    * Add step to save output to a local .html file until sendgrid is fixed

--- src/scanner.py
+++ src/scanner.py
@@ -15,77 +15,81 @@ from sendgrid.helpers.mail import *
 project = "aj-cloud-staging"
 region = "asia-south1"
 
-scanner = nmap.PortScanner()  # instantiate nmap.PortScanner object
-
-# data = {
-#     "35.200.203.245": {
-#         "ports": ["tcp/22\tstate:open", "tcp/80\tstate:open"]
-#     }
-#
-# }
-
 
 def get_instance_ip_address_list():
-    print("\nAccessing google client API\n")
+    print("\nAccessing Google Cloud API")
     compute = googleapiclient.discovery.build('compute', 'v1')
 
-    print("Getting Addresses")
+    print("Getting IP Addresses ... \n")
+
     request = compute.addresses().list(project=project, region=region)
     api_result = request.execute()
     return api_result
 
 
 def execute_port_scan(api_result):
-    data = {}
-    for instance in api_result['items']:
-        scanner.scan(instance['address'], '1-65535')
-        print(scanner.command_line())
-        for host in scanner.all_hosts():
-            data[host] = {}
-            print('-----------------------------------------------------------------')
-            print('Host : %s (%s)' % (host, scanner[host].hostname()))
+    line_break = '-----------------------------------------------------------------'
+    data = []
 
-            for proto in scanner[host].all_protocols():
-                print('--------------------')
-                print('Protocol : %s' % proto)
-                ports = scanner[host][proto]
+    items = api_result['items']
+    print_ip_metadata(items)
 
-                data[host]['ports'] = []
-                for port in ports:
-                    state = scanner[host][proto][port]['state']
-                    port_data = '%s:%s\t state: %s' % (proto, port, state)
-                    print(port_data)
-                    data[host]['ports'].append(port_data)
-        print('----------------------------------------------------')
-        print("\n")
+    for instance in items:
+        ip_address = instance['address']
+        # Optimizing scan to check 10,000 ports since the ones above are rarely used
+        port_range = '1-10000'
 
+        scanner = nmap.PortScanner()  # instantiate nmap.PortScanner object
+        scanner.scan(ip_address, port_range)
+        print("Command: ", scanner.command_line())
+
+        # Key = IP Address
+        # Value = List of ports scanned
+        ip_address_data = {}
+
+        hosts_list = scanner.all_hosts()
+        print("Host scanned: ", hosts_list)
+        if len(hosts_list) <= 0:
+            print("Host data is missing. "
+                  "Host may be down / Scan may have failed or aborted.")
+            continue
+
+        for host in hosts_list:
+            ip_address_data = {host: []}
+            print(line_break)
+            host_ = scanner[host]
+            hostname = host_.hostname()
+            print('Host : %s (%s)' % (host, hostname))
+
+            for protocol in host_.all_protocols():
+                print(line_break)
+                protocol_ = host_[protocol]
+
+                for port in protocol_:
+                    state = protocol_[port]['state']
+                    state_label = 'state: %s' % state
+
+                    port_label = '%s:%s' % (protocol, port)
+
+                    port_data = {port_label: state}
+                    print(state_label + " state:" + port_label)
+                    ip_address_data[host].append(port_data)
+
+            print(line_break)
+
+        data.append(ip_address_data)
+
+    return data
+
+
+def render_output(data):
     env = Environment(loader=FileSystemLoader('.'))
     template = env.get_template("sample_format.html")
     template_vars = {
-        "title": "Port Scanner",
+        "title": "Port Scanner results",
         "data": data
     }
     return template.render(template_vars)
-
-
-# def execute_port_scan(api_result):
-#     if api_result is not None:
-#         ip_list = api_result['items']
-#
-#         print_ip_metadata(ip_list)
-#
-#         for item in ip_list:
-#             ip = item['address']
-#             command = "nmap -sV -p 1-10 " + ip + " -Pn -oX output.xml"
-#             # os.system('output="$(' + command + ')"; echo $output >> output.txt; echo "\n" >> output.txt')
-#             os.system('output="$(' + command + ')"; ')
-#
-#         print("Completed scans of all IPs. Please find report in output.txt")
-#         return True
-#     else:
-#         print("IP addresses couldn't be retrieved... Error occurred")
-#         print(api_result)
-#         return False
 
 
 def print_ip_metadata(ip_list):
@@ -95,6 +99,14 @@ def print_ip_metadata(ip_list):
         name = item['name']
         instance_id = item['id']
         print("Instance: " + name + "  -  IP Address: " + ip + "  -  ID: " + instance_id)
+
+
+def write_output(output_content):
+    print(output_content)
+    with open("output.html", "w+") as output_file:
+        output_file.seek(0)
+        output_file.write(output_content)
+        print("Saved successfully in file: output.html")
 
 
 def send_report(report_content):
@@ -119,10 +131,15 @@ def send_report(report_content):
 
 if __name__ == "__main__":
     api_response = get_instance_ip_address_list()
-    scan_success = execute_port_scan(api_response)
-    print(scan_success)
-    # if scan_success:
-    #     content = get_output_content()
-    #     send_report(content)
-    # else:
-    #     print("Scanning Failed!")
+    if api_response is None:
+        print("Failed to get IP Addresses. Aborting scan")
+        exit(1)
+
+    scan_data = execute_port_scan(api_response)
+    if len(scan_data) < 1:
+        print("Port scanning failed. Exiting.")
+        exit(1)
+
+    scan_output = render_output(scan_data)
+    write_output(scan_output)
+    # send_report(scan_output)
